98ccb79d8abb495455675e4c055e18c4abc4f18b
S4vyss <mlodywojownik345@gmail.com>
2023-06-28 12:59:47 +0200

    Initial frontend commit

---
+++ pricaution.frontend/src/app/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import styles from './page.module.css'
+import {Container, Typography} from "@mui/material";
+
+export default function Home() {
+  return (
+    <Container>
+      <Typography variant="h1">
+        siema
+      </Typography>
+    </Container>
+  )
+}
