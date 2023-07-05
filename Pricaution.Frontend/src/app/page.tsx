5118174efe6ab9b31771afe4265d573f43cb0c8f
Tobaritto <andzyg1509@gmail.com>
2023-07-05 22:23:02 +0200

    Created Header, cleared globals.css and set background color

--- Pricaution.Frontend/src/app/page.tsx
+++ Pricaution.Frontend/src/app/page.tsx
@@ -1,9 +1,10 @@
 "use client"
 import {Container, Typography} from "@mui/material";
-
+import Header from "./components/header/Header";
 export default function Home() {
   return (
     <Container>
+        <Header />
       <Typography variant="h1">
           Be precautious with your purchase
       </Typography>
