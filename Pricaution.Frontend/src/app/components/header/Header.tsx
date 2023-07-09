22712d5a69b5ed3a1be1c741031bf641e697adb5
Tobaritto <andzyg1509@gmail.com>
2023-07-09 14:50:21 +0200

    Installed mui icons, created other pages files and added theme changing button.

--- Pricaution.Frontend/src/app/components/header/Header.tsx
+++ Pricaution.Frontend/src/app/components/header/Header.tsx
@@ -1,7 +1,15 @@
-import {AppBar, Container, Box,  Button, Typography} from "@mui/material";
+import {AppBar, Container, Box, Button, Typography, IconButton} from "@mui/material";
 import Image from "next/image";
+import DarkMode from "@mui/icons-material/DarkMode";
+import LightMode from "@mui/icons-material/LightMode";
 
 const pages = ['Home Page','About us','Contact'];
+let darkMode = false;
+
+const darkModeChange = () => {
+    !darkMode
+}
+
 export default function Header() {
 
     return(
@@ -13,11 +21,14 @@ export default function Header() {
                     </Typography>
                     {pages.map((item) => (
                         <Button key={item}>
-                            <Typography component="a" href="/" sx={{ color: '#000', fontSize: "20px"}}>
+                            <Typography key="href" component="a" href="/" sx={{ color: '#000', fontSize: "20px"}}>
                                 {item}
                             </Typography>
                         </Button>
                     ))}
+                    <IconButton size="large" onClick={darkModeChange}>
+                        {darkMode? <DarkMode />:<LightMode />}
+                    </IconButton>
                 </Box>
             </Container>
         </AppBar>
---
+++ Pricaution.Frontend/src/app/pages/AboutUs.tsx
@@ -0,0 +1,8 @@
+import Header from "../components/header/Header";
+
+export default function About() {
+
+    return(
+        <Header />
+    )
+}
